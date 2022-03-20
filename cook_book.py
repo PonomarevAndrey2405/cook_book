56066180c34be64e3109079a18c137d5662a7ed1
Пономарёв Андрей <damned01@yandex.ru>
2022-03-20 14:15:21 +0500

    step1

---
+++ cook_book.py
@@ -0,0 +1,17 @@
+import json
+
+def open_file():
+    with open('recipes.txt') as file:
+        cook_book = {}
+        for line in file:
+            line = line.strip()
+            cook_book.update({line: []})
+            k = int(file.readline().strip())
+            for _ in range(k):
+                lst = file.readline().strip().split(' | ')
+                dict = {'ingredient_name': lst[0], 'quantity': lst[1], 'measure': lst[2]}
+                cook_book[line].append(dict)
+            file.readline()
+    return cook_book
+
+print(json.dumps(open_file()))
